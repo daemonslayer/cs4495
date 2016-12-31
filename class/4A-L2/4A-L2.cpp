c74288565d7b1f0c29a63e6ce5b859c036ffde0e
daemonSlayer <kpgupta98@gmail.com>
2016-12-31 12:45:46 +0530

    lec source code [cpp]

---
+++ class/4A-L2/4A-L2.cpp
@@ -0,0 +1,21 @@
+/**
+ * Author: Kapil Gupta <kpgupta98@gmail.com>
+ * Organization: XantheLabs
+ * Created: December 2016
+ */
+
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, char const *argv[]) {
+  
+  return 0;
+}
